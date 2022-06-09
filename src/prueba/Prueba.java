dd1a061ff9dd5fc769a8689f3115dc7f29144d75
oscar <oscar@BoaHancockMaster>
2022-06-08 18:27:58 -0600

    cambio v2
    agregue tres linea der print

--- src/prueba/Prueba.java
+++ src/prueba/Prueba.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package prueba;
 
 /**
@@ -15,6 +12,9 @@ public class Prueba {
      */
     public static void main(String[] args) {
      System.out.println("Hola mundo");
+      System.out.println(" mundo");
+       System.out.println("Hola ");
+        System.out.println("yt");
     }
     
 }
